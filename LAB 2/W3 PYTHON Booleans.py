3ab46aec14abc44a7ff049bd5bf823415c824bc9
Ralphboi <114404552+Ralphboi@users.noreply.github.com>
2024-01-24 20:14:55 +0600

    Add files via upload

---
+++ LAB 2/W3 PYTHON Booleans.py
@@ -0,0 +1,20 @@
+#Ex.1
+print(10 > 9)
+#ANSWER: True
+
+#Ex.2
+print(10 == 9)
+#ANSWER: False
+
+#Ex.3
+print(10 < 9)
+#ANSWER: False
+
+#Ex.4
+print(bool("abc"))
+#ANSWER: True
+
+#Ex.5
+print(bool(0))
+#ANSWER: False
+
